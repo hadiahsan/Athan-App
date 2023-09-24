a557e20baa9788bac27bf6393b80d049b68d22f5
Hadi Ahsan <71375374+HadeyAhsan@users.noreply.github.com>
2023-09-24 19:41:07 -0400

    Adds mp3 file and requirements.txt

---
+++ athan_software/src/main.py
@@ -0,0 +1,14 @@
+from prayer_times import get_prayer_times
+from notification import schedule_athan
+
+latitude = "43.489690"
+longitude = "-79.883970"
+method = ""  # Refer to the API documentation for available methods
+athan_file = "../assets/athan.mp3"
+
+def main():
+    timings = get_prayer_times(latitude, longitude, method)
+    schedule_athan(timings, athan_file)
+
+if __name__ == "__main__":
+    main()
